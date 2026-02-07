07cd47d63f0faea4028c0980b2723369787969f8
Dipanshu <dipanshu639722@gmail.com>
2026-02-07 13:48:00 +0530

    Some changes in main file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt  
 
-fig,ax=plt.subplot()
+fig, ax=plt.subplots()
 
 fruits=['apple','blueberry','cherry','orange']
 counts=[140,10,30,55]
@@ -19,7 +19,7 @@ cat=["bored","happy",'happy','happy','happy','bored']
 dog=['bored','bored','bored','happy','bored','bored']
 activity=["combing",'drinking','feeding','napping','playing','washing']
 
-fig,ax=plt.subplot()
+fig,ax=plt.subplots()
 ax.plot(activity,dog,label="dog")
 ax.plot(activity,cat,label="cat")
 ax.legend()
